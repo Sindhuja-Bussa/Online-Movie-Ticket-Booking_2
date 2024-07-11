fffe80cb22784f46d13ee1b6f89dd6ccdb9c742c
Sindhuja Bussa <sindhubussa03@gmail.com>
2024-07-11 17:28:46 +0530

    Update Ds_project.c

--- Ds_project.c
+++ Ds_project.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-// Struct to store customer details
+int c, t1, t2, t3, s, ch1, a, i, j, ar[100], n, ns;
+
 struct details {
     char first_name[100];
     char last_name[100];
@@ -16,103 +16,291 @@ struct details {
     struct details* next;
 };
 
-// Function prototypes
-void location(struct details** head);
-struct details* read(struct details* head);
-void display(struct details* head);
-void s_display();
-void bill(struct details* head);
-void load_data(struct details** head);
-void save_data(struct details* head);
+// Define a head pointer for the linked list
+struct details* head = NULL;
 
-int c, t1, t2, t3, s, ch1, a, i, j, ar[100], n, ns;
+// Function prototypes
+void location(void);
+struct details* read();
+void display();
+void s_display();
+void bill();
+void add_customer(struct details* new_customer);
+void display_all_customers();
 
 int main() {
-    struct details* head = NULL;
-    load_data(&head);  // Load existing data from file
-    location(&head);
-    save_data(head);   // Save data to file before exiting
+    location();
     return 0;
 }
 
-void location(struct details** head) {
-    printf("\t \t \t \t \t \t------  WELCOME  ------ \t \t \t \t \t \t ");
+void location(void) {
+    printf("\t\t\t\t\t\t\t------  WELCOME  ------\t\t\t\t\t\t\t");
     printf("\n SELECT CITY:");
-    printf("\n\t \t 1:WARANGAL \n\t \t 2:HANAMKONDA \n \t \t 3:KAZIPET");
+    printf("\n\t\t 1:WARANGAL \n\t\t 2:HANAMKONDA \n\t\t 3:KAZIPET");
     scanf("%d", &c);
-    switch(c) {
+    switch (c) {
         case 1:
             printf("\n SELECT THEATRE:");
-            printf("\n\t \t  1:PVR CINEMAS \n\t \t  2:ASIAN GEMINI \n\t \t  3:RAM LAXMAN \n\t \t  4:RADHIKA");
+            printf("\n\t\t  1:PVR CINEMAS \n\t\t  2:ASIAN GEMINI \n\t\t  3:RAM LAXMAN \n\t\t  4:RADHIKA");
             scanf("%d", &t1);
+            switch (t1) {
+                case 1:
+                    printf("\n SELECT MOVIE:");
+                    printf("\n\t\t  1:RRR \n\t\t  2:KGF-2 \n\t\t  3:DR STRANGE \n\t\t  4:F3");
+                    scanf("%d", &s);
+                    switch (s) {
+                        case 1:
+                            printf("\nRRR\n");
+                            s_display();
+                            break;
+                        case 2:
+                            printf("\nKGF-2\n");
+                            s_display();
+                            break;
+                        case 3:
+                            printf("\nDR STRANGE\n");
+                            s_display();
+                            break;
+                        case 4:
+                            printf("\nF3\n");
+                            s_display();
+                            break;
+                    }
+                    break;
+                case 2:
+                    printf("\n SELECT MOVIE:");
+                    printf("\n\t\t  1:RRR \n\t\t  2:KGF-2 \n\t\t  3:DR STRANGE \n\t\t  4:F3");
+                    scanf("%d", &s);
+                    switch (s) {
+                        case 1:
+                            printf("\nRRR\n");
+                            s_display();
+                            break;
+                        case 2:
+                            printf("\nKGF-2\n");
+                            s_display();
+                            break;
+                        case 3:
+                            printf("\nDR STRANGE\n");
+                            s_display();
+                            break;
+                        case 4:
+                            printf("\nF3\n");
+                            s_display();
+                            break;
+                    }
+                    break;
+
+                case 3:
+                    printf("\n SELECT MOVIE:");
+                    printf("\n\t\t  1:RRR \n\t\t  2:KGF-2 \n\t\t  3:F3");
+                    scanf("%d", &s);
+                    switch (s) {
+                        case 1:
+                            printf("\nRRR\n");
+                            s_display();
+                            break;
+                        case 2:
+                            printf("\nKGF-2\n");
+                            s_display();
+                            break;
+                        case 3:
+                            printf("\nF3\n");
+                            s_display();
+                            break;
+                    }
+                    break;
+
+                case 4:
+                    printf("\n SELECT MOVIE:");
+                    printf("\n\t\t  1:KGF-2 \n\t\t  2:F3");
+                    scanf("%d", &s);
+                    switch (s) {
+                        case 1:
+                            printf("\nKGF-2\n");
+                            s_display();
+                            break;
+                        case 2:
+                            printf("\nF3\n");
+                            s_display();
+                            break;
+                    }
+                    break;
+            }
             break;
         case 2:
             printf("\n SELECT THEATRE:");
-            printf("\n\t \t  1:ASIAN SRIDEVI MULTIPLEX \n\t \t  2:ASHOKA \n\t \t  3:AMRUTHA");
+            printf("\n\t\t  1:ASIAN SRIDEVI MULTIPLEX \n\t\t  2:ASHOKA \n\t\t  3:AMRUTHA");
             scanf("%d", &t2);
+            switch (t2) {
+                case 1:
+                    printf("\n SELECT MOVIE:");
+                    printf("\n\t\t  1:RRR \n\t\t  2:KGF-2 \n\t\t  3:DR STRANGE \n\t\t  4:F3");
+                    scanf("%d", &s);
+                    switch (s) {
+                        case 1:
+                            printf("\nRRR\n");
+                            s_display();
+                            break;
+                        case 2:
+                            printf("\nKGF-2\n");
+                            s_display();
+                            break;
+                        case 3:
+                            printf("\nDR STRANGE\n");
+                            s_display();
+                            break;
+                        case 4:
+                            printf("\nF3\n");
+                            s_display();
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    printf("\n SELECT MOVIE:");
+                    printf("\n\t\t  1:RRR \n\t\t  2:KGF-2 \n\t\t  3:F3");
+                    scanf("%d", &s);
+                    switch (s) {
+                        case 1:
+                            printf("\nRRR\n");
+                            s_display();
+                            break;
+                        case 2:
+                            printf("\nKGF-2\n");
+                            s_display();
+                            break;
+                        case 3:
+                            printf("\nF3\n");
+                            s_display();
+                            break;
+                    }
+                    break;
+
+                case 3:
+                    printf("\n SELECT MOVIE:");
+                    printf("\n\t\t  1:KGF-2 \n\t\t  2:F3");
+                    scanf("%d", &s);
+                    switch (s) {
+                        case 1:
+                            printf("\nKGF-2\n");
+                            s_display();
+                            break;
+                        case 2:
+                            printf("\nF3\n");
+                            s_display();
+                            break;
+                    }
+                    break;
+            }
             break;
         case 3:
             printf("\n SELECT THEATRE:");
-            printf("\n\t \t  1:BHAVANI\n\t \t  2:SHAMBHAVI");
+            printf("\n\t\t  1:BHAVANI\n\t\t  2:SHAMBHAVI");
             scanf("%d", &t3);
-            break;
-    }
+            switch (t3) {
+                case 1:
+                    printf("\n SELECT MOVIE:");
+                    printf("\n\t\t  1:RRR \n\t\t  2:KGF-2 \n\t\t  3:F3");
+                    scanf("%d", &s);
+                    switch (s) {
+                        case 1:
+                            printf("\nRRR\n");
+                            s_display();
+                            break;
+                        case 2:
+                            printf("\nKGF-2\n");
+                            s_display();
+                            break;
+                        case 3:
+                            printf("\nF3\n");
+                            s_display();
+                            break;
+                    }
+                    break;
 
-    printf("\n SELECT MOVIE:");
-    printf("\n\t \t  1:RRR \n\t \t  2:KGF-2 \n\t \t  3:DR STRANGE \n\t \t  4:F3");
-    scanf("%d", &s);
-    switch(s) {
-        case 1:
-            printf("\nRRR\n");
-            s_display();
-            break;
-        case 2:
-            printf("\nKGF-2\n");
-            s_display();
-            break;
-        case 3:
-            printf("\nDR STRANGE\n");
-            s_display();
-            break;
-        case 4:
-            printf("\nF3\n");
-            s_display();
-            break;
+                case 2:
+                    printf("\n SELECT MOVIE:");
+                    printf("\n\t\t  1:KGF-2 \n\t\t  2:F3");
+                    scanf("%d", &s);
+                    switch (s) {
+                        case 1:
+                            printf("\nKGF-2\n");
+                            s_display();
+                            break;
+                        case 2:
+                            printf("\nF3\n");
+                            s_display();
+                            break;
+                    }
+                    break;
+            }
     }
-
     printf("\n ENTER YOUR CHOICE:");
     printf("\n 1:TO ENTER YOUR DETAILS \n 2:TO DISPLAY ALL THE DETAILS");
     scanf("%d", &a);
-    switch(a) {
-        case 1:
-            *head = read(*head);
-            bill(*head);
+    switch (a) {
+        case 1: {
+            struct details* new_customer = read();
+            add_customer(new_customer);
+            bill();
             break;
+        }
         case 2:
-            display(*head);
+            display_all_customers();
             break;
     }
 }
 
-struct details* read(struct details* head) {
-    struct details* new_node = (struct details*)malloc(sizeof(struct details));
-    if (!new_node) {
-        printf("Memory allocation error\n");
-        return head;
+struct details* read() {
+    struct details* new_customer = (struct details*)malloc(sizeof(struct details));
+    if (new_customer == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
     }
+
     printf("ENTER THE FOLLOWING DETAILS:\n FIRST NAME \n LAST NAME \n AADHAR NUMBER \n CREDIT NUMBER \n EXPIRY MONTH \n EXPIRY YEAR \n CARD ID \n CONTACT NUMBER");
-    scanf("%s %s %lld %lld %d %ld %d %lld", new_node->first_name, new_node->last_name, &new_node->aadhar_number, &new_node->credit_number, &new_node->expiry_month, &new_node->expiry_year, &new_node->card_id, &new_node->contact_number);
-    new_node->next = head;
-    return new_node;
+    scanf("%s %s %lld %lld %d %ld %d %lld",
+          new_customer->first_name,
+          new_customer->last_name,
+          &new_customer->aadhar_number,
+          &new_customer->credit_number,
+          &new_customer->expiry_month,
+          &new_customer->expiry_year,
+          &new_customer->card_id,
+          &new_customer->contact_number);
+
+    new_customer->next = NULL;
+
+    // Save details to file
+    FILE* de = fopen("DETAILS_OF_CUSTOMER", "a");
+    if (de != NULL) {
+        fprintf(de, "\n%s \n%s \n%lld \n%lld \n%d \n%ld \n%d \n%lld",
+                new_customer->first_name,
+                new_customer->last_name,
+                new_customer->aadhar_number,
+                new_customer->credit_number,
+                new_customer->expiry_month,
+                new_customer->expiry_year,
+                new_customer->card_id,
+                new_customer->contact_number);
+        fclose(de);
+    }
+
+    return new_customer;
 }
 
-void display(struct details* head) {
-    struct details* temp = head;
+void display(struct details* person) {
     printf("\n THE DETAILS YOU HAVE ENTERED:");
-    while (temp != NULL) {
-        printf("\n\n\n%s \n%s \n%lld \n%lld \n%d \n%ld \n%d \n%lld", temp->first_name, temp->last_name, temp->aadhar_number, temp->credit_number, temp->expiry_month, temp->expiry_year, temp->card_id, temp->contact_number);
-        temp = temp->next;
-    }
+    printf("\n%s \n%s \n%lld \n%lld \n%d \n%ld \n%d \n%lld",
+           person->first_name,
+           person->last_name,
+           person->aadhar_number,
+           person->credit_number,
+           person->expiry_month,
+           person->expiry_year,
+           person->card_id,
+           person->contact_number);
 }
 
 void s_display() {
@@ -126,21 +314,20 @@ void s_display() {
     printf("\n enter the no of seats you want to select:");
     scanf("%d", &n);
 
-    printf("\n enter the  seat number you want to select:");
+    printf("\n enter the seat number you want to select:");
     for (i = 0; i < n; i++)
-        fscanf(stdin, "%d", &ns);
+        scanf("%d", &ns);
 }
 
-void bill(struct details* head) {
-    struct details* temp = head;
+void bill() {
     printf("\n--------------------------------------------------------------------------------");
     printf("\n|                                                                              |");
     printf("\n|                                                                              |");
-    printf("\n| NAME OF THE CUSTOMER: %s %s                                                   |", temp->first_name, temp->last_name);
+    printf("\n| NAME OF THE CUSTOMER: %s %s                                                   |", head->first_name, head->last_name);
     printf("\n|                                                                              |");
     printf("\n| COST OF 1 TICKET: 150                                                         |");
     printf("\n|                                                                              |");
-    printf("\n| NO OF TICKETS: %d                                                              |", n);
+    printf("\n| NO OF TICKETS: %d                                                             |", n);
     printf("\n|                                                                              |");
     printf("\n| TOTAL COST: %d                                                               |", 150 * n);
     printf("\n|                                                                              |");
@@ -153,40 +340,27 @@ void bill(struct details* head) {
     printf("\n--------------------------------------------------------------------------------");
 }
 
-void load_data(struct details** head) {
-    FILE* file = fopen("customer_details.txt", "r");
-    if (!file) {
-        return; // No existing data
-    }
-
-    struct details* temp;
-    while (1) {
-        temp = (struct details*)malloc(sizeof(struct details));
-        if (!temp) {
-            printf("Memory allocation error\n");
-            break;
+void add_customer(struct details* new_customer) {
+    if (head == NULL) {
+        head = new_customer;
+    } else {
+        struct details* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
         }
-        if (fscanf(file, "%s %s %lld %lld %d %ld %d %lld %d", temp->first_name, temp->last_name, &temp->aadhar_number, &temp->credit_number, &temp->expiry_month, &temp->expiry_year, &temp->card_id, &temp->contact_number, &temp->ns) == EOF) {
-            free(temp);
-            break;
-        }
-        temp->next = *head;
-        *head = temp;
+        temp->next = new_customer;
     }
-    fclose(file);
 }
 
-void save_data(struct details* head) {
-    FILE* file = fopen("customer_details.txt", "w");
-    if (!file) {
-        printf("Error opening file for writing\n");
+void display_all_customers() {
+    struct details* temp = head;
+    if (temp == NULL) {
+        printf("No customer details to display.\n");
         return;
     }
 
-    struct details* temp = head;
-    while (temp) {
-        fprintf(file, "%s %s %lld %lld %d %ld %d %lld %d\n", temp->first_name, temp->last_name, temp->aadhar_number, temp->credit_number, temp->expiry_month, temp->expiry_year, temp->card_id, temp->contact_number, temp->ns);
+    while (temp != NULL) {
+        display(temp);
         temp = temp->next;
     }
-    fclose(file);
 }
